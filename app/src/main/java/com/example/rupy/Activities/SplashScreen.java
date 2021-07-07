3a9c3f96d5ad7445a54d36c9159b498a186ac231
VISHAL <vishal53804@gmail.com>
2021-07-07 14:06:42 +0530

    initial commit

---
+++ app/src/main/java/com/example/rupy/Activities/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.rupy.Activities;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.rupy.R;
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        //Used for starting the main Activity after SplashScreen animation(after a delay of 2000 millis)
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Home.class));
+                finish();
+            }
+        }, 2500);
+    }
+}
